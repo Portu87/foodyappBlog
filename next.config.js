bea69aa198f12a501d1b204a429be5ed31ecb361
Portu87 <sebas.p.g87@gmail.com>
2023-09-19 20:26:16 +0200

    12

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -6,3 +8,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+// const withMDX = createMDX({
+//   options: {
+//     extension: /\.mdx?$/,
+//     remarkPlugins: [remarkGfm],
+//     rehypePlugins: [],
+//     // If you use `MDXProvider`, uncomment the following line.
+//     // providerImportSource: "@mdx-js/react",
+//   },
+// })
+// export default withMDX(nextConfig)
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,60 +1,62 @@
-import PageHeader from "@/components/PageHeader";
-import { fetchApi } from "@/helpers/fetch-api";
-import { formatDate } from "@/helpers/format-date-helper";
-import { Post } from "@/interfaces/post";
-import { notFound } from "next/navigation";
-import Image from "next/image";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import ButtonBack from "@/components/ButtonBack";
-const getData = async (slug = "") => {
-  const path = "/posts";
-  const urlParamsObject = {
-    populate: "*",
-    filters: {
-      slug: slug,
-    },
-  };
-  const { data } = await fetchApi(path, urlParamsObject);
-  return data[0];
-};
+ import PageHeader from "@/components/PageHeader";
+ import { fetchApi } from "@/helpers/fetch-api";
+ import { formatDate } from "@/helpers/format-date-helper";
+ import { Post } from "@/interfaces/post";
+ import { notFound } from "next/navigation";
+ import Image from "next/image";
+ import ButtonBack from "@/components/ButtonBack";
 
-interface Props {
-  params: {
-    slug: string;
-  };
-}
-const Post = async ({ params }: Props) => {
-  const { slug } = params;
-  const post: Post = await getData(slug);
+ const getData = async (slug = "") => {
+   const path = "/posts";
+   const urlParamsObject = {
+     populate: "*",
+     filters: {
+       slug: slug,
+     },
+   };
+   const { data } = await fetchApi(path, urlParamsObject);
+   return data[0];
+ };
 
-  if (!post) {
-    return notFound();
-  }
 
-  const { title, description, body, createdAt, image } = post.attributes;
-  const { url, width, height } = image.data.attributes.formats.medium;
+ interface Props {
+   params: {
+     slug: string;
+   };
+ }
+ const Post = async ({ params }: Props) => {
+   const { slug } = params;
+   const post: Post = await getData(slug);
 
-  return (
-    <div className="space-y-8 px-8">
-      <div className="ml-8">
-      <ButtonBack children="Volver"/>
-      </div>
-      <PageHeader text={title} />
-      <p className="text-gray-500">{formatDate(createdAt)}</p>
-      <p>{description}</p>
-      <Image
-        className="rounded-t-lg"
-        src={url}
-        alt={`image ${title}`}
-        width={width}
-        height={height}
-      />
-      <div className="prose">
-        {/* @ts-expect-error Server Component */}
-        <MDXRemote source={body} />
-      </div>
-    </div>
-  );
-};
+   if (!post) {
+     return notFound();
+   }
 
-export default Post;
+   const { title, description, body, createdAt, image } = post.attributes;
+   const { url, width, height } = image.data.attributes.formats.medium;
+
+ 
+   return (
+     <div className="space-y-8 px-8">
+       <div className="ml-8">
+       <ButtonBack children="Volver"/>
+       </div>
+       <PageHeader text={title} />
+       <p className="text-gray-500">{formatDate(createdAt)}</p>
+       <p>{description}</p>
+       <Image
+         className="rounded-t-lg"
+         src={url}
+         alt={`image ${title}`}
+         width={width}
+         height={height}
+       />
+       <div className="prose">    
+       {body} 
+         {/* <MDXRemote {...compiledSource} /> */}
+       </div>
+     </div>
+   );
+ };
+
+ export default Post;

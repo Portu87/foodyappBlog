01e49650a75f28327c7e1cf9aca0d45a185efcf5
Portu87 <sebas.p.g87@gmail.com>
2023-09-20 16:41:23 +0200

    20-09-2023/16-41

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
   
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 space-y-8">
       <Link children="Ir al Blog" href={'/blog'} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"/>
+      <Link children="Ir al Store" href={'/store'} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"/>
     </main>
   )
 }
